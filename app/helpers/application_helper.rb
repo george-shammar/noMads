8dff4864a84d33ca822ae0de7c93a32bfa38d8dd
george-shammar <george.gbenle@outlook.com>
2021-03-23 21:34:09 +0000

    Resize navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,15 +9,15 @@ module ApplicationHelper
       end
     
       def navbar_button_sign_out
-        link_to 'Sign out', destroy_user_session_path, method: :delete, class: "btn btn-light mx-1" if user_signed_in?
+        link_to 'Sign out', destroy_user_session_path, method: :delete, class: "btn btn-light mx-1 btn-sm" if user_signed_in?
       end
     
       def navbar_button_sign_up
-        link_to 'Sign up', new_user_registration_path, class: "btn btn-light mx-1" unless user_signed_in?
+        link_to 'Sign up', new_user_registration_path, class: "btn btn-light mx-1 btn-sm" unless user_signed_in?
       end
     
       def navbar_button_log_in
-        link_to 'Sign in', new_user_session_path, class: "btn btn-light mx-1" unless user_signed_in?
+        link_to 'Sign in', new_user_session_path, class: "btn btn-light mx-1 btn-sm" unless user_signed_in?
       end
     
       def user_signed_in?
