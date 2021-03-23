df0bcda934041f96b8411b1916e0ac8891cf30f0
george-shammar <george.gbenle@outlook.com>
2021-03-23 17:55:50 +0000

    Create registration controller

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,6 +1,7 @@
 class OpinionsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  
   before_action :set_opinion, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /opinions or /opinions.json
   def index
