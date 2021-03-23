80a02c10143578b5794f91e9c586bffb05e6e878
george-shammar <george.gbenle@outlook.com>
2021-03-23 11:42:43 +0000

    Change vertical nav style

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,4 +1,5 @@
 class OpinionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_opinion, only: %i[ show edit update destroy ]
 
   # GET /opinions or /opinions.json
