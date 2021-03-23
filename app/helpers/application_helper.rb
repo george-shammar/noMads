e7d8ed828e074b03b25042cb994726408ad9045a
george-shammar <george.gbenle@outlook.com>
2021-03-23 20:47:49 +0000

    create user features o index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+
+      def navbar_button_name(user)
+        link_to user.fullname, edit_user_registration_path, class: 'btn btn-light mx-1' if user_signed_in?
+      end
+    
+      def navbar_button_home
+        link_to 'noMads', root_path, class: 'btn btn-primary mx-1'
+      end
+    
+      def navbar_button_sign_out
+        link_to 'Sign out', destroy_user_session_path, method: :delete, class: 'btn btn-primary mx-1' if user_signed_in?
+      end
+    
+      def navbar_button_sign_up
+        link_to 'Sign up', new_user_registration_path, class: 'btn btn-primary mx-1' unless user_signed_in?
+      end
+    
+      def navbar_button_log_in
+        link_to 'Sign in', new_user_session_path, class: 'btn btn-primary mx-1' unless user_signed_in?
+      end
+    
+      def user_signed_in?
+        !current_user.nil?
+      end
 end
