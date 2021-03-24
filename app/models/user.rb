0a5b39b0d466f274ee42e6d5d9fc4cfac8663465
george-shammar <george.gbenle@outlook.com>
2021-03-24 13:48:53 +0000

    Add has many association to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   has_many :opinions, class_name: 'Opinion', foreign_key: 'authorid', dependent: :destroy
   has_many :follower_relationships, class_name: 'Following', foreign_key: 'followerid', dependent: :destroy
   has_many :followed_relationships, class_name: 'FOllowing', foreign_key: 'followedid', dependent: :destroy
+  has_many :follower_users, through: :followed_relationships, source: :follower_user
+  has_many :followed_users, through: :follower_relationships, source: :followed_user
 end
