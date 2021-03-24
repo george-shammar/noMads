11c8181fc18064de105b2c0678a31e1c4a3a046e
george-shammar <george.gbenle@outlook.com>
2021-03-24 17:12:47 +0000

    Implement follower count and following count

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,4 +1,14 @@
 class Following < ApplicationRecord
     belongs_to :follower_user, class_name: 'User', foreign_key: 'followerid'
     belongs_to :followed_user, class_name: 'User', foreign_key: 'followedid'
+
+
+    def follower_count(current_user)
+        @follower_count = Following.where(followedid: current_user.id).count
+    end
+
+    def following_count(current_user)
+        @following_count = Following.where(followerid: current_user.id).count
+    end
 end
+
