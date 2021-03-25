affbceb9ed172bb22e551ac02fc20dbc0a936e55
george-shammar <george.gbenle@outlook.com>
2021-03-25 13:06:59 +0000

    Create user's show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :opinions
   resources :followings, only: [:create, :show, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show]
 
   root 'opinions#index'
 end
