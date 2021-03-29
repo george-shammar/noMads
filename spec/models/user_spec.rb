7e498e26a70b149f6840081af46fc93d60a336b3
george-shammar <george.gbenle@outlook.com>
2021-03-29 21:54:58 +0000

    Unit test for username validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,6 @@ RSpec.describe User, type: :model do
     it { should have_many(:followed_users) }
     it { should have_one_attached(:image) }
     it { should have_one_attached(:coverimage) }
+
+    it { should validate_presence_of(:username) }
 end
