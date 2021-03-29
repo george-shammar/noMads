00b7357141d4dfc12b2b57f294ee4893cc66e7ee
george-shammar <george.gbenle@outlook.com>
2021-03-29 21:42:52 +0000

    Unit test for user association has many follower_relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
     it { should have_many(:opinions) }
     it { should have_many(:likes) }
+    it { should have_many(:follower_relationships) }
 end
