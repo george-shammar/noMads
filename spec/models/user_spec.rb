4739df2e3116c3e4210552591a47203d954a0c85
george-shammar <george.gbenle@outlook.com>
2021-03-29 21:48:19 +0000

    Add unit test for users has many follower_users association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe User, type: :model do
     it { should have_many(:likes) }
     it { should have_many(:follower_relationships) }
     it { should have_many(:followed_relationships) }
+    it { should have_many(:follower_users) }
 end
