1efc3d8d65f22eefa364d38a1b8064eaf5746192
george-shammar <george.gbenle@outlook.com>
2021-03-29 21:51:10 +0000

    Unit test for user has one attached image association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,5 @@ RSpec.describe User, type: :model do
     it { should have_many(:followed_relationships) }
     it { should have_many(:follower_users) }
     it { should have_many(:followed_users) }
+    it { should have_one_attached(:image) }
 end
